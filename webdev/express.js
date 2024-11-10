bcafe0a56987427ea8d1dced9f0bbbdc36c53cae
Anshgupta0123 <gupta2004ansh@gmail.com>
2024-11-10 23:49:07 +0530

    Create express.js

---
+++ webdev/express.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.get("/health.checkup",function(req , res){
+    const username = req.header.username;
+    const password =req.header.password;
+    const kidneyid = req.header.kidneyid;
+
+    if(password !="Ansh"&& password!="admin"){
+        if(kidneyid ==1 || kidneyid ==2)
+        res.json({
+            msg : "kidney fine"
+        })
+    }else{
+        res.json({
+            msg : "invalid input"
+        })
+    }
+
+    res.status(400).json({"msg":"something is bad"})
+});
+
+app.listen(300);
